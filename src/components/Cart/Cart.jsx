59d8e6c1fafd061349dc6d31f6217fdbb806ccac
hossainta1 <tanvir.hossain303030@gmail.com>
2023-09-10 12:11:46 +0600

    Added total

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = () => {
+const Cart = ({ cart }) => {
+
+    let total = 0;
+
+    for (const product of cart) {
+        total = total + product.price;
+    }
+
     return (
-        <div>
-
+        <div className='cart'>
+            <h4>Order Summary</h4>
+            <p>Selected Items: {cart.length}</p>
+            <p>Total Price :{total}</p>
+            <p>Total Shipping :</p>
+            <p>Tax:</p>
+            <h6>Grand Total:</h6>
         </div>
     );
 };
